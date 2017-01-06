003f8362bc05ad910c02bf8dd945ed54d9254e4c
Ernie <ernie@localhost.localdomain>
2017-01-06 10:51:56 -0500

    finsish routing

---
+++ src/app/hero-detail.component.ts
@@ -0,0 +1,78 @@
+import {Component, OnInit, Input} from '@angular/core';
+import {Hero} from "./hero";
+import {ActivatedRoute, Params} from "@angular/router";
+import {Location} from "@angular/common";
+import {HeroService} from "./hero.service";
+
+import 'rxjs/add/operator/switchMap';
+
+@Component({
+  selector: 'my-hero-detail',
+  template: `
+    <div *ngIf="hero">
+      <h2>{{hero.name}} details</h2>
+      <div><label>id: </label>{{hero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="hero.name" palceholder="name">
+      </div>
+      <button (click)="goBack()">Back</button>
+    </div>
+    
+  `,
+  styles: [`
+label {
+  display: inline-block;
+  width: 3em;
+  margin: .5em 0;
+  color: #607D8B;
+  font-weight: bold;
+}
+input {
+  height: 2em;
+  font-size: 1em;
+  padding-left: .4em;
+}
+button {
+  margin-top: 20px;
+  font-family: Arial;
+  background-color: #eee;
+  border: none;
+  padding: 5px 10px;
+  border-radius: 4px;
+  cursor: pointer; cursor: hand;
+}
+button:hover {
+  background-color: #cfd8dc;
+}
+button:disabled {
+  background-color: #eee;
+  color: #ccc; 
+  cursor: auto;
+}
+
+`
+
+  ]
+})
+export class HeroDetailComponent implements OnInit {
+
+  hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
+
+  ngOnInit() {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+}
